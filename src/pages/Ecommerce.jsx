afb179b14db109a58a5c2eb74591d2ec4f1234e6
saba-iram-n <iramsaba340@gmail.com>
2025-02-01 14:46:06 +0530

    second

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -307,8 +307,8 @@ const Ecommerce = () => {
               src={product9}
               alt=""
             />
-             {/* <div className="mt-8">
-              <p className="font-semibold text-lg">React 18 coming soon!</p>
+             <div className="mt-8">
+              <p className="font-semibold text-lg">Workout tips</p>
               <p className="text-gray-400 ">By Johnathan Doe</p>
               <p className="mt-8 text-sm text-gray-400">
                 This will be the small description for the news you have shown
@@ -322,7 +322,7 @@ const Ecommerce = () => {
                   borderRadius="10px"
                 />
               </div> 
-            </div>  */}
+            </div> 
           </div>
         </div>
       </div>
